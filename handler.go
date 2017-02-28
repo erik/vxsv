8041a3832f888f507f072b369b59267d4b70c82b
Erik Price <github@erikprice.net>
2017-02-27 17:46:33 -0800

    WIP refactor into separate handler class

---
+++ handler.go
@@ -0,0 +1,326 @@
+// ui mode specific handlers
+
+package main
+
+import (
+	"encoding/json"
+	"sort"
+	"strconv"
+	"strings"
+
+	"fmt"
+	"github.com/nsf/termbox-go"
+)
+
+type ModeHandler interface {
+	HandleKey(ev termbox.Event)
+	Repaint()
+}
+
+type HandlerDefault struct {
+	ui *UI
+}
+
+func (h HandlerDefault) Repaint() {
+	ui := h.ui
+	_, height := termbox.Size()
+
+	first := ui.offsetY
+	last := clamp(ui.offsetY+height, 0, len(ui.filterMatches))
+	total := len(ui.filterMatches) - 1
+	filter := ""
+
+	if ui.filterString != "" {
+		filter = fmt.Sprintf("[filter: \"%s\"] ", ui.filterString)
+	}
+
+	line := fmt.Sprintf("%s[rows %d-%d of %d] :%d, %d", filter, first, last, total, ui.offsetX, ui.offsetY)
+	writeLine(0, height-1, termbox.ColorDefault, termbox.ColorDefault, line)
+}
+
+func (h HandlerDefault) HandleKey(ev termbox.Event) {
+	ui := h.ui
+	vw, vh := ui.viewSize()
+
+	maxYOffset := clamp(len(ui.filterMatches)-(vh-2), 0, len(ui.filterMatches)-1)
+	endOfLine := ui.endOfLine()
+
+	switch {
+	case ev.Key == termbox.KeyCtrlL:
+		termbox.Sync()
+	case ev.Key == termbox.KeyCtrlA:
+		ui.offsetX = 0
+	case ev.Key == termbox.KeyCtrlE:
+		ui.offsetX = -endOfLine + vw
+	case ev.Key == termbox.KeyArrowRight:
+		ui.offsetX = clamp(ui.offsetX-5, -endOfLine+vw, 0)
+	case ev.Key == termbox.KeyArrowLeft:
+		ui.offsetX = clamp(ui.offsetX+5, -endOfLine+vw, 0)
+	case ev.Key == termbox.KeyArrowUp:
+		ui.offsetY = clamp(ui.offsetY-1, 0, maxYOffset)
+	case ev.Key == termbox.KeyArrowDown:
+		ui.offsetY = clamp(ui.offsetY+1, 0, maxYOffset)
+	case ev.Ch == '/', ev.Key == termbox.KeyCtrlR:
+		ui.handler = HandlerFilter{h}
+		ui.offsetY = 0
+	case ev.Key == termbox.KeyEnter:
+		jsonObj := make(map[string]interface{})
+
+		row := ui.rows[ui.offsetY]
+		for i, col := range ui.columns {
+			str := row[i]
+
+			if v, err := strconv.ParseInt(str, 10, 64); err == nil {
+				jsonObj[col] = v
+			} else if v, err := strconv.ParseFloat(str, 64); err == nil {
+				jsonObj[col] = v
+			} else if v, err := strconv.ParseBool(str); err == nil {
+				jsonObj[col] = v
+			} else {
+				jsonObj[col] = str
+			}
+
+		}
+
+		jsonStr, err := json.MarshalIndent(jsonObj, "", "  ")
+		// TODO: This should be handled
+		if err != nil {
+			panic(err)
+		}
+
+		ui.handler = NewPopup(h.ui, string(jsonStr))
+
+	case ev.Key == termbox.KeySpace:
+		ui.offsetY = clamp(ui.offsetY+vh, 0, maxYOffset)
+	case ev.Ch == 'C':
+		ui.handler = HandlerColumnSelect{h, 0}
+		ui.offsetX = 0
+		ui.colIdx = 0
+	case ev.Ch == 'G':
+		ui.offsetY = maxYOffset
+	case ev.Ch == 'g':
+		ui.offsetY = 0
+	case ev.Ch == 'Z':
+		ui.zebraStripe = !ui.zebraStripe
+	case ev.Ch == 'X':
+		for i, _ := range ui.columnOpts {
+			ui.columnOpts[i].expanded = !globalExpanded
+			// FIXME: Possibly not the best behavior
+			ui.columnOpts[i].collapsed = false
+		}
+		globalExpanded = !globalExpanded
+	case ev.Ch == '?':
+		ui.handler = NewPopup(h.ui, HELP_TEXT)
+	}
+}
+
+type HandlerFilter struct {
+	HandlerDefault
+}
+
+func (h HandlerFilter) Repaint() {
+	ui := h.ui
+	_, height := termbox.Size()
+
+	line := fmt.Sprintf("FILTER [%d matches]: %s", len(ui.filterMatches), ui.filterString)
+	writeLine(0, height-1, termbox.ColorWhite|termbox.AttrBold, termbox.ColorDefault, line)
+	termbox.SetCursor(len(line), height-1)
+
+}
+
+func (h HandlerFilter) HandleKey(ev termbox.Event) {
+	ui := h.ui
+
+	// Ch == 0 implies this was a special key
+	if ev.Ch == 0 && ev.Key != termbox.KeySpace {
+		if ev.Key == termbox.KeyEsc || ev.Key == termbox.KeyCtrlG {
+			ui.switchToDefault()
+
+			ui.filterString = ""
+			ui.filterRows(false)
+		} else if ev.Key == termbox.KeyEnter {
+			ui.switchToDefault()
+		} else if ev.Key == termbox.KeyDelete || ev.Key == termbox.KeyBackspace ||
+			ev.Key == termbox.KeyBackspace2 {
+			if sz := len(ui.filterString); sz > 0 {
+				ui.filterString = ui.filterString[:sz-1]
+				ui.filterRows(false)
+			}
+		} else if ev.Key == termbox.KeyCtrlW || ev.Key == termbox.KeyCtrlU {
+			ui.filterString = ""
+			ui.filterRows(false)
+		} else {
+			// Fallback to default handling for arrows etc
+			HandlerDefault{h.ui}.HandleKey(ev)
+		}
+		return
+	}
+
+	if ev.Key == termbox.KeySpace {
+		ui.filterString += " "
+	} else {
+		ui.filterString += string(ev.Ch)
+	}
+
+	ui.offsetY = 0
+	ui.filterRows(true)
+}
+
+func (ui *UI) rowSorter(i, j int) bool {
+	row1 := ui.rows[ui.filterMatches[i]]
+	row2 := ui.rows[ui.filterMatches[j]]
+
+	v1, err1 := strconv.ParseFloat(row1[ui.colIdx], 32)
+	v2, err2 := strconv.ParseFloat(row2[ui.colIdx], 32)
+
+	if err1 == nil && err2 == nil {
+		return v1 < v2
+	}
+
+	return row1[ui.colIdx] < row2[ui.colIdx]
+}
+
+type HandlerColumnSelect struct {
+	HandlerDefault
+	column int
+}
+
+func (h HandlerColumnSelect) Repaint() {
+	ui := h.ui
+	_, height := termbox.Size()
+	line := "COLUMN SELECT (^g quit) [" + ui.columns[ui.colIdx] + "]"
+	writeLine(0, height-1, termbox.ColorWhite|termbox.AttrBold, termbox.ColorDefault, line)
+}
+
+func (h HandlerColumnSelect) HandleKey(ev termbox.Event) {
+	ui := h.ui
+
+	switch {
+	case ev.Key == termbox.KeyCtrlA:
+		ui.colIdx = 0
+	case ev.Key == termbox.KeyCtrlE:
+		ui.colIdx = len(ui.columns) - 1
+	case ev.Key == termbox.KeyArrowRight:
+		next := ui.findNextColumn(ui.colIdx, 1)
+		ui.colIdx = clamp(next, 0, len(ui.columns)-1)
+	case ev.Key == termbox.KeyArrowLeft:
+		next := ui.findNextColumn(ui.colIdx, -1)
+		ui.colIdx = clamp(next, 0, len(ui.columns)-1)
+	case ev.Ch == '<':
+		sort.SliceStable(ui.filterMatches, func(i, j int) bool {
+			return ui.rowSorter(i, j)
+		})
+	case ev.Ch == '>':
+		sort.SliceStable(ui.filterMatches, func(i, j int) bool {
+			return ui.rowSorter(j, i)
+		})
+	case ev.Ch == 'w':
+		ui.columnOpts[ui.colIdx].collapsed = !ui.columnOpts[ui.colIdx].collapsed
+	case ev.Ch == 'x':
+		ui.columnOpts[ui.colIdx].expanded = !ui.columnOpts[ui.colIdx].expanded
+		if ui.columnOpts[ui.colIdx].expanded {
+			ui.columnOpts[ui.colIdx].collapsed = false
+		}
+	case ev.Ch == '.':
+		ui.columnOpts[ui.colIdx].pinned = !ui.columnOpts[ui.colIdx].pinned
+
+		if ui.columnOpts[ui.colIdx].pinned {
+			ui.columnOpts[ui.colIdx].collapsed = false
+		}
+
+	case ev.Key == termbox.KeyCtrlG, ev.Key == termbox.KeyEsc:
+		ui.handler = HandlerDefault{h.ui}
+	default:
+		HandlerDefault{h.ui}.HandleKey(ev)
+	}
+
+	// find if we've gone off screen and readjust
+	// TODO: this bit is buggy
+	cursorPosition := 0
+	for i, _ := range ui.columns {
+		colOpts := ui.columnOpts[i]
+
+		if i == ui.colIdx {
+			break
+		}
+		//cursorPosition += 3
+		if !colOpts.collapsed {
+			cursorPosition += colOpts.width
+		}
+	}
+
+	width, _ := termbox.Size()
+	if cursorPosition > width-ui.offsetX || cursorPosition < -ui.offsetX {
+		ui.offsetX = -cursorPosition
+	}
+}
+
+type HandlerPopup struct {
+	HandlerDefault
+
+	content          []string
+	offsetX, offsetY int
+}
+
+func NewPopup(ui *UI, content string) HandlerPopup {
+	return HandlerPopup{
+		HandlerDefault: HandlerDefault{ui},
+		content:        strings.Split(content, "\n"),
+	}
+}
+
+func (h HandlerPopup) Repaint() {
+	width, height := termbox.Size()
+
+	popupW := clamp(120, 40, width-5)
+	popupH := clamp(len(h.content)+2, 10, height-5)
+
+	x := width/2 - popupW/2
+	y := height/2 - popupH/2
+
+	fmtString := "%s%-" + strconv.Itoa(popupW) + "s%s"
+
+	borders := [][]string{
+		[]string{"┌─", "─┐"},
+		[]string{"│ ", " │"},
+		[]string{"└─", "─┘"},
+	}
+
+	for i := -1; i <= popupH; i += 1 {
+		var border []string
+		var content string
+
+		if i == -1 {
+			border = borders[0]
+			content = strings.Repeat("─", popupW)
+		} else if i < popupH {
+			border = borders[1]
+			if i < len(h.content) {
+				content = h.content[i]
+			} else {
+				content = " "
+			}
+		} else {
+			border = borders[2]
+			content = strings.Repeat("─", popupW)
+		}
+
+		line := fmt.Sprintf(fmtString, border[0], content, border[1])
+		writeString(x, y+i, termbox.ColorWhite, termbox.ColorDefault, line)
+	}
+
+	line := "POPUP (^g quit)"
+	writeLine(0, height-1, termbox.ColorWhite|termbox.AttrBold, termbox.ColorDefault, line)
+
+}
+
+func (h HandlerPopup) HandleKey(ev termbox.Event) {
+
+	if ev.Key == termbox.KeyEsc || ev.Key == termbox.KeyCtrlG || ev.Ch == 'q' {
+		h.ui.SetHandler(HandlerDefault{h.ui})
+	}
+}
+
+type HandlerRowSelect struct {
+	HandlerDefault
+}
--- ui.go
+++ ui.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 
@@ -58,16 +56,6 @@ FILTER MODE
   [ENTER]         apply filter and return to default mode
 `
 
-type inputMode int
-
-const (
-	ModeDefault = iota
-	ModeFilter
-	ModeColumnSelect
-	ModeRowSelect
-	ModePopup
-)
-
 type columnOptions struct {
 	expanded  bool
 	collapsed bool
@@ -117,13 +105,8 @@ func (f columnFilter) matches(row []string) bool {
 	return strings.Contains(lowerCol, lowerFilter)
 }
 
-type popup struct {
-	content          []string
-	offsetX, offsetY int
-}
-
 type UI struct {
-	mode             inputMode
+	handler          ModeHandler
 	rowIdx, colIdx   int // Selection control
 	offsetX, offsetY int // Pan control
 	filters          []filter
@@ -134,7 +117,6 @@ type UI struct {
 	columns          []string
 	rows             [][]string
 	width            int
-	popup            popup
 }
 
 // It is so dumb that go doesn't have this
@@ -178,54 +160,13 @@ func writeLine(x, y int, fg, bg termbox.Attribute, line string) {
 	}
 }
 
-func (p *popup) repaint() {
-	w, h := termbox.Size()
-
-	popupW := clamp(120, 40, w-5)
-	popupH := clamp(len(p.content)+2, 10, h-5)
-
-	x := w/2 - popupW/2
-	y := h/2 - popupH/2
-
-	fmtString := "%s%-" + strconv.Itoa(popupW) + "s%s"
-
-	borders := [][]string{
-		[]string{"┌─", "─┐"},
-		[]string{"│ ", " │"},
-		[]string{"└─", "─┘"},
-	}
-
-	for i := -1; i <= popupH; i += 1 {
-		var border []string
-		var content string
-
-		if i == -1 {
-			border = borders[0]
-			content = strings.Repeat("─", popupW)
-		} else if i < popupH {
-			border = borders[1]
-			if i < len(p.content) {
-				content = p.content[i]
-			} else {
-				content = " "
-			}
-		} else {
-			border = borders[2]
-			content = strings.Repeat("─", popupW)
-		}
-
-		line := fmt.Sprintf(fmtString, border[0], content, border[1])
-		writeString(x, y+i, termbox.ColorWhite, termbox.ColorDefault, line)
-	}
-}
-
 var cellFmtString = "%-" + strconv.Itoa(MAX_CELL_WIDTH) + "s"
 
 func (ui *UI) writeCell(cell string, x, y, index, pinBound int, fg, bg termbox.Attribute) int {
 	colOpts := ui.columnOpts[index]
 	lastCol := index == len(ui.columnOpts)-1
 
-	if index == ui.colIdx && ui.mode == ModeColumnSelect {
+	if index == ui.colIdx {
 		fg = HILITE_FG
 		bg = HILITE_BG
 	}
@@ -303,7 +244,7 @@ func (ui *UI) writeRow(x, y int, row []string) {
 	}
 }
 
-func NewUi(data TabularData) UI {
+func NewUi(data TabularData) *UI {
 	colOpts := make([]columnOptions, len(data.Columns))
 	columns := make([]string, len(data.Columns))
 	filterMatches := make([]int, len(data.Rows))
@@ -322,10 +263,9 @@ func NewUi(data TabularData) UI {
 		filterMatches[i] = i
 	}
 
-	return UI{
+	ui := &UI{
 		offsetX:       0,
 		offsetY:       0,
-		mode:          ModeDefault,
 		colIdx:        -1,
 		columnOpts:    colOpts,
 		rows:          data.Rows,
@@ -336,6 +276,10 @@ func NewUi(data TabularData) UI {
 		filterString:  "",
 		filterMatches: filterMatches,
 	}
+
+	ui.switchToDefault()
+
+	return ui
 }
 
 func (ui *UI) Init() error {
@@ -344,7 +288,6 @@ func (ui *UI) Init() error {
 	}
 
 	termbox.SetInputMode(termbox.InputEsc)
-
 	return nil
 }
 
@@ -361,16 +304,7 @@ eventloop:
 				break eventloop
 			}
 
-			switch ui.mode {
-			case ModeFilter:
-				ui.handleKeyFilter(ev)
-			case ModeColumnSelect:
-				ui.handleKeyColumnSelect(ev)
-			case ModePopup:
-				ui.handleKeyPopup(ev)
-			default:
-				ui.handleKeyDefault(ev)
-			}
+			ui.handler.HandleKey(ev)
 		}
 
 		ui.repaint()
@@ -414,7 +348,6 @@ func (ui *UI) filterRows(narrowing bool) {
 func (ui *UI) repaint() {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 	termbox.HideCursor()
-	_, height := termbox.Size()
 	_, vh := ui.viewSize()
 
 	const coldef = termbox.ColorDefault
@@ -429,32 +362,7 @@ func (ui *UI) repaint() {
 		}
 	}
 
-	switch ui.mode {
-	case ModeFilter:
-		line := fmt.Sprintf("FILTER [%d matches]: %s", len(ui.filterMatches), ui.filterString)
-		writeLine(0, height-1, termbox.ColorWhite|termbox.AttrBold, termbox.ColorDefault, line)
-		termbox.SetCursor(len(line), height-1)
-	case ModeColumnSelect:
-		line := "COLUMN SELECT (^g quit) [" + ui.columns[ui.colIdx] + "]"
-		writeLine(0, height-1, termbox.ColorWhite|termbox.AttrBold, termbox.ColorDefault, line)
-	case ModePopup:
-		ui.popup.repaint()
-		line := "POPUP (^g quit)"
-		writeLine(0, height-1, termbox.ColorWhite|termbox.AttrBold, termbox.ColorDefault, line)
-	default:
-		first := ui.offsetY
-		last := clamp(ui.offsetY+height, 0, len(ui.filterMatches))
-		total := len(ui.filterMatches) - 1
-		filter := ""
-
-		if ui.filterString != "" {
-			filter = fmt.Sprintf("[filter: \"%s\"] ", ui.filterString)
-		}
-
-		line := fmt.Sprintf("%s[rows %d-%d of %d] :", filter, first, last, total)
-		writeLine(0, height-1, termbox.ColorDefault, termbox.ColorDefault, line)
-	}
-
+	ui.handler.Repaint()
 	termbox.Flush()
 }
 
@@ -495,41 +403,6 @@ func (ui *UI) panTo(col int) {
 
 }
 
-func (ui *UI) handleKeyFilter(ev termbox.Event) {
-	// Ch == 0 implies this was a special key
-	if ev.Ch == 0 && ev.Key != termbox.KeySpace {
-		if ev.Key == termbox.KeyEsc || ev.Key == termbox.KeyCtrlG {
-			ui.mode = ModeDefault
-			ui.filterString = ""
-			ui.filterRows(false)
-		} else if ev.Key == termbox.KeyEnter {
-			ui.mode = ModeDefault
-		} else if ev.Key == termbox.KeyDelete || ev.Key == termbox.KeyBackspace ||
-			ev.Key == termbox.KeyBackspace2 {
-			if sz := len(ui.filterString); sz > 0 {
-				ui.filterString = ui.filterString[:sz-1]
-				ui.filterRows(false)
-			}
-		} else if ev.Key == termbox.KeyCtrlW || ev.Key == termbox.KeyCtrlU {
-			ui.filterString = ""
-			ui.filterRows(false)
-		} else {
-			// Fallback to default handling for arrows etc
-			ui.handleKeyDefault(ev)
-		}
-		return
-	}
-
-	if ev.Key == termbox.KeySpace {
-		ui.filterString += " "
-	} else {
-		ui.filterString += string(ev.Ch)
-	}
-
-	ui.offsetY = 0
-	ui.filterRows(true)
-}
-
 var globalExpanded = false
 
 func (ui *UI) findNextColumn(current, direction int) int {
@@ -561,167 +434,10 @@ func (ui *UI) findNextColumn(current, direction int) int {
 	return current
 }
 
-func (ui *UI) rowSorter(i, j int) bool {
-	row1 := ui.rows[ui.filterMatches[i]]
-	row2 := ui.rows[ui.filterMatches[j]]
-
-	v1, err1 := strconv.ParseFloat(row1[ui.colIdx], 32)
-	v2, err2 := strconv.ParseFloat(row2[ui.colIdx], 32)
-
-	if err1 == nil && err2 == nil {
-		return v1 < v2
-	}
-
-	return row1[ui.colIdx] < row2[ui.colIdx]
+func (ui *UI) switchToDefault() {
+	ui.handler = HandlerDefault{ui}
 }
 
-func (ui *UI) handleKeyColumnSelect(ev termbox.Event) {
-	switch {
-	case ev.Key == termbox.KeyCtrlA:
-		ui.colIdx = 0
-	case ev.Key == termbox.KeyCtrlE:
-		ui.colIdx = len(ui.columns) - 1
-	case ev.Key == termbox.KeyArrowRight:
-		next := ui.findNextColumn(ui.colIdx, 1)
-		ui.colIdx = clamp(next, 0, len(ui.columns)-1)
-	case ev.Key == termbox.KeyArrowLeft:
-		next := ui.findNextColumn(ui.colIdx, -1)
-		ui.colIdx = clamp(next, 0, len(ui.columns)-1)
-	case ev.Ch == '<':
-		sort.SliceStable(ui.filterMatches, func(i, j int) bool {
-			return ui.rowSorter(i, j)
-		})
-	case ev.Ch == '>':
-		sort.SliceStable(ui.filterMatches, func(i, j int) bool {
-			return ui.rowSorter(j, i)
-		})
-	case ev.Ch == 'w':
-		ui.columnOpts[ui.colIdx].collapsed = !ui.columnOpts[ui.colIdx].collapsed
-	case ev.Ch == 'x':
-		ui.columnOpts[ui.colIdx].expanded = !ui.columnOpts[ui.colIdx].expanded
-		if ui.columnOpts[ui.colIdx].expanded {
-			ui.columnOpts[ui.colIdx].collapsed = false
-		}
-	case ev.Ch == '.':
-		ui.columnOpts[ui.colIdx].pinned = !ui.columnOpts[ui.colIdx].pinned
-
-		if ui.columnOpts[ui.colIdx].pinned {
-			ui.columnOpts[ui.colIdx].collapsed = false
-		}
-
-	case ev.Key == termbox.KeyCtrlG, ev.Key == termbox.KeyEsc:
-		ui.mode = ModeDefault
-	default:
-		ui.handleKeyDefault(ev)
-	}
-
-	// find if we've gone off screen and readjust
-	// TODO: this bit is buggy
-	cursorPosition := 0
-	for i, _ := range ui.columns {
-		colOpts := ui.columnOpts[i]
-
-		if i == ui.colIdx {
-			break
-		}
-		//cursorPosition += 3
-		if !colOpts.collapsed {
-			cursorPosition += colOpts.width
-		}
-	}
-
-	width, _ := termbox.Size()
-	if cursorPosition > width-ui.offsetX || cursorPosition < -ui.offsetX {
-		ui.offsetX = -cursorPosition
-	}
-}
-
-func (ui *UI) handleKeyDefault(ev termbox.Event) {
-	vw, vh := ui.viewSize()
-
-	maxYOffset := clamp(len(ui.filterMatches)-(vh-2), 0, len(ui.filterMatches)-1)
-	endOfLine := ui.endOfLine()
-
-	switch {
-	case ev.Key == termbox.KeyCtrlL:
-		termbox.Sync()
-	case ev.Key == termbox.KeyCtrlA:
-		ui.offsetX = 0
-	case ev.Key == termbox.KeyCtrlE:
-		ui.offsetX = -endOfLine + vw
-	case ev.Key == termbox.KeyArrowRight:
-		ui.offsetX = clamp(ui.offsetX-5, -endOfLine+vw, 0)
-	case ev.Key == termbox.KeyArrowLeft:
-		ui.offsetX = clamp(ui.offsetX+5, -endOfLine+vw, 0)
-	case ev.Key == termbox.KeyArrowUp:
-		ui.offsetY = clamp(ui.offsetY-1, 0, maxYOffset)
-	case ev.Key == termbox.KeyArrowDown:
-		ui.offsetY = clamp(ui.offsetY+1, 0, maxYOffset)
-	case ev.Ch == '/', ev.Key == termbox.KeyCtrlR:
-		ui.mode = ModeFilter
-		ui.offsetY = 0
-	case ev.Key == termbox.KeyEnter:
-		jsonObj := make(map[string]interface{})
-
-		row := ui.rows[ui.offsetY]
-		for i, col := range ui.columns {
-			str := row[i]
-
-			if v, err := strconv.ParseInt(str, 10, 64); err == nil {
-				jsonObj[col] = v
-			} else if v, err := strconv.ParseFloat(str, 64); err == nil {
-				jsonObj[col] = v
-			} else if v, err := strconv.ParseBool(str); err == nil {
-				jsonObj[col] = v
-			} else {
-				jsonObj[col] = str
-			}
-
-		}
-
-		jsonStr, err := json.MarshalIndent(jsonObj, "", "  ")
-		// TODO: This should be handled
-		if err != nil {
-			panic(err)
-		}
-
-		ui.mode = ModePopup
-		ui.popup = popup{
-			content: strings.Split(string(jsonStr), "\n"),
-		}
-
-	case ev.Key == termbox.KeySpace:
-		ui.offsetY = clamp(ui.offsetY+vh, 0, maxYOffset)
-	case ev.Ch == 'C':
-		ui.mode = ModeColumnSelect
-		ui.offsetX = 0
-		ui.colIdx = 0
-	case ev.Ch == 'G':
-		ui.offsetY = maxYOffset
-	case ev.Ch == 'g':
-		ui.offsetY = 0
-	case ev.Ch == 'Z':
-		ui.zebraStripe = !ui.zebraStripe
-	case ev.Ch == 'X':
-		for i, _ := range ui.columnOpts {
-			ui.columnOpts[i].expanded = !globalExpanded
-			// FIXME: Possibly not the best behavior
-			ui.columnOpts[i].collapsed = false
-		}
-		globalExpanded = !globalExpanded
-	case ev.Ch == '?':
-		ui.mode = ModePopup
-		ui.popup = popup{
-			content: strings.Split(HELP_TEXT, "\n"),
-		}
-
-	case ui.mode == ModeDefault && ev.Ch == 'q':
-		panic("TODO: real exit")
-	}
-}
-
-func (ui *UI) handleKeyPopup(ev termbox.Event) {
-	if ev.Key == termbox.KeyEsc || ev.Key == termbox.KeyCtrlG || ev.Ch == 'q' {
-		ui.mode = ModeDefault
-	}
+func (ui *UI) SetHandler(h ModeHandler) {
+	ui.handler = h
 }
